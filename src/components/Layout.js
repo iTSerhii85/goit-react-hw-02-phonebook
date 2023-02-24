3d34d8f4ee90869a3133d79608154b372fc2e47b
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-02-24 23:41:53 +0200

    chenge style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,17 @@ import styled from 'styled-components';
 
 export const Layout = styled.div`
   padding: 16px 50px;
+  width: 650px;
+  margin-left: auto;
+  margin-right: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
+  margin-top: 15px;
+
+  box-shadow: 6px 7px 43px 4px rgba(0,0,0,1);
 
   background: rgb(251,239,0);
   background: -moz-linear-gradient(320deg, rgba(251,239,0,1) 0%, rgba(0,18,246,1) 100%);
